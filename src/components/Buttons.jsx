5a6b91c647089d4835106f2b6e1fceca37da2488
Faris Irfan <farisirfanbit19.03@gmail.com>
2024-10-23 11:39:59 +0500

    commit

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Button = ({ text, variant, onClick }) => {
   let buttonClasses =
-    "h-12 p-3 md:mt-7 mt-2 w-fit flex items-center justify-center font-sans transition-transform hover:scale-95 font-semibold rounded-lg text-base cursor-pointer ";
+    "h-fit py-3 px-6 md:mt-7 mt-2 w-full flex items-center justify-center font-sans transition-transform hover:scale-95 font-semibold rounded-lg text-xs cursor-pointer ";
 
   if (variant === "primary") {
     buttonClasses += "bg-[#f2f2f2] text-black";
   } else if (variant === "secondary") {
-    buttonClasses += "border-2 border-[#f2f2f2] text-[#f2f2f2]";
+    buttonClasses += "border border-[#f2f2f2] text-[#f2f2f2]";
   }
 
   return (
